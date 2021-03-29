d673a988ea05549bac58f66d9522748420f0f3f8
lozzoc <reach.maddaloc@gmail.com>
2021-03-29 13:31:10 -0400

    added super sweet way of mocking usestate

--- src/JottoInput.js
+++ src/JottoInput.js
@@ -2,8 +2,26 @@ import React from "react"
 import PropTypes from "prop-types"
 
 function JottoInput({secretWord}){
+  const [text,setText] = React.useState("")
   return (
-    <div data-test="component-input" />
+    <div data-test="component-input" >
+      <form className="form-inline">
+      <input
+        data-test="input-box"
+        className="mb-2 mx-sm-3"
+        type="text"
+        value={text}
+        onChange={(event)=> setText(event.target.value)}
+        placeholder="enter guess"
+        />
+        <button
+          data-test="submit-button"
+          className="btn btn-primary mb-2"
+          >
+          submit
+        </button>
+      </form>
+    </div>
   )
 }
 
--- src/JottoInput.test.js
+++ src/JottoInput.test.js
@@ -7,9 +7,8 @@ const defaultProps = [{
   secretWord: ""
 }]
 
-const setup = (props={}) => {
-  let el = {...defaultProps, ...props}
-  return shallow(<JottoInput {...el}/>)
+const setup = (secretWord="react") => {
+  return shallow(<JottoInput secretWord={secretWord}/>)
 }
 
 
@@ -18,4 +17,22 @@ test("should render with no err",function(){
   let wrapper = setup()
   let input = findByTestAttr(wrapper,"component-input")
   expect(input.length).toBe(1)
+})
+
+
+describe("state controlled input field",function(){
+  test("state updates with value of inputbox cahnge",function(){
+    const mockSetCurrentGuess = jest.fn()
+    React.useState = jest.fn(function(){
+      return ["",mockSetCurrentGuess]
+    })
+    let wrapper = setup()
+    let inputBox = findByTestAttr(wrapper,"input-box")
+    let mockEvent = {target: { value: "train"} }
+    inputBox.simulate("change",mockEvent)
+
+    expect(mockSetCurrentGuess).toHaveBeenCalledWith("train")
+
+
+  })
 })
